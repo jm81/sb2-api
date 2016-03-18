cf821bfda2b33ed67e3067f4dc134e0e69bf3f3d
Jared E Morgan <jmorgan@mchost.net>
2016-03-18 14:20:01 -0500

    spec ControllerMacros - support for requiring profile

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -11,11 +11,21 @@ module ControllerMacros
       FactoryGirl.create :auth_token, user: profile_member.member_user
     end
 
+    let(:session_profile) do
+      session_auth_token.user.default_profile
+    end
+
     set_auth_token_header
   end
 
   def use_auth_token_without_profile
     let(:session_auth_token) { FactoryGirl.create :auth_token }
     set_auth_token_header
+  end
+
+  def clear_auth_token
+    before(:each) do
+      request.headers['Authorization'] = nil
+    end
   end
 end
